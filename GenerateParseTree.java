877f67e4292d21fb0eaa083604a5233c4c74cb25
Filipergv <varjaofilipe@gmail.com>
2015-09-18 15:37:07 -0300

    prints out the parse tree

--- GenerateParseTree.java
+++ GenerateParseTree.java
@@ -1,9 +1,18 @@
 import org.antlr.v4.runtime.*;
 import org.antlr.v4.runtime.tree.*;
+import java.io.FileInputStream;
+import java.io.InputStream;
 
 public class GenerateParseTree {
     public static void main(String[] args) throws Exception {
-        ANTLRInputStream input = new ANTLRInputStream(System.in);
+
+        String inputFile = null;
+
+        if (args.length > 0) inputFile = args[0];
+        InputStream is = System.in;
+        if ( inputFile!=null ) is = new FileInputStream(inputFile);
+
+        ANTLRInputStream input = new ANTLRInputStream(is);
         CerlLexer lexer = new CerlLexer(input);
         CommonTokenStream tokens = new CommonTokenStream(lexer);
         CerlParser parser =  new CerlParser(tokens);
